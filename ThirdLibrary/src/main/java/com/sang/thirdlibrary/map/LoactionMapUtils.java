1cf74edefcda40add36e2afe3301805f0570fdac
pingyapeng <pingyapeng@ehousechina.com>
2018-08-08 16:43:25 +0800

    定位初步完成

---
+++ ThirdLibrary/src/main/java/com/sang/thirdlibrary/map/LoactionMapUtils.java
@@ -0,0 +1,74 @@
+package com.sang.thirdlibrary.map;
+
+import android.location.Location;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.MyLocationStyle;
+
+/**
+ * 作者： ${PING} on 2018/8/8.
+ */
+public class LoactionMapUtils implements AMap.OnMyLocationChangeListener {
+
+    private AMap aMap;
+    private MyLocationStyle myLocationStyle;
+    private UiSettings mUiSettings;//定义一个UiSettings对象
+    private AMap.OnMyLocationChangeListener onMyLocationChangeListener;
+
+
+    private static class InnerMpa {
+        public static LoactionMapUtils utils = new LoactionMapUtils();
+    }
+
+    private LoactionMapUtils() {
+    }
+
+    public static LoactionMapUtils getInstance() {
+        return InnerMpa.utils;
+    }
+
+    public LoactionMapUtils initMap(MapView mMapView) {
+        aMap = mMapView.getMap();
+
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+//        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        myLocationStyle.showMyLocation(true);//设置是否显示定位小蓝点，用于满足只想使用定位，不想使用定位小蓝点的场景，设置false以后图面上不再有定位蓝点的概念，但是会持续回调位置信息。
+
+
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//定位一次，且将视角移动到地图中心点。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) ;//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE);//连续定位、且将视角移动到地图中心点，地图依照设备方向旋转，定位点会跟随设备移动。（1秒1次定位）
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+//以下三种模式从5.1.0版本开始提供
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，地图依照设备方向旋转，并且蓝点会跟随设备移动。
+
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        mUiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+        mUiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+
+        aMap.setMyLocationEnabled(true);// 可触发定位并显示当前位置
+
+        aMap.setOnMyLocationChangeListener(this);
+        return this;
+
+    }
+
+    public void setOnMyLocationChangeListener(AMap.OnMyLocationChangeListener listener) {
+        this.onMyLocationChangeListener=listener;
+    }
+    @Override
+    public void onMyLocationChange(Location location) {
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+        if (onMyLocationChangeListener!=null){
+            onMyLocationChangeListener.onMyLocationChange(location);
+        }
+    }
+}
--- ThirdLibrary/src/main/java/com/sang/thirdlibrary/map/ui/MapLoactionFragment.java
+++ ThirdLibrary/src/main/java/com/sang/thirdlibrary/map/ui/MapLoactionFragment.java
@@ -5,34 +5,48 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.BitmapDescriptor;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+import com.sang.common.utils.JLog;
+import com.sang.common.utils.ToastUtils;
 import com.sang.thirdlibrary.R;
+import com.sang.thirdlibrary.map.LoactionMapUtils;
 import com.sang.thirdlibrary.utils.XLog;
 
 /**
  * 作者： ${PING} on 2018/8/8.
- *
+ * <p>
  * 地图定位App，用来查询并显示地点的的Fragment
- *
  */
-public class MapLoactionFragment extends Fragment implements AMap.OnMyLocationChangeListener {
+public class MapLoactionFragment extends Fragment implements AMap.OnMyLocationChangeListener, PoiSearch.OnPoiSearchListener {
 
 
     MapView mMapView = null;
-    AMap aMap;
+    private EditText etSearch;
+    private PoiSearch.Query query;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View inflate = inflater.inflate(R.layout.map_loaction_fragment, container, false);
-        mMapView = (MapView) inflate.findViewById(R.id.map);
+        mMapView = inflate.findViewById(R.id.map);
+        etSearch = inflate.findViewById(R.id.et_search);
         return inflate;
     }
 
@@ -40,6 +54,7 @@ public class MapLoactionFragment extends Fragment implements AMap.OnMyLocationCh
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mMapView.onCreate(savedInstanceState);
+        LoactionMapUtils.getInstance().initMap(mMapView).setOnMyLocationChangeListener(this);
         initView();
     }
 
@@ -49,18 +64,21 @@ public class MapLoactionFragment extends Fragment implements AMap.OnMyLocationCh
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
     }
+
     @Override
     public void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
         mMapView.onResume();
     }
+
     @Override
     public void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
         mMapView.onPause();
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -68,46 +86,53 @@ public class MapLoactionFragment extends Fragment implements AMap.OnMyLocationCh
         mMapView.onSaveInstanceState(outState);
     }
 
-    MyLocationStyle myLocationStyle;
 
-    private void initView(){
-        if (aMap == null) {
-            aMap = mMapView.getMap();
+    private void initView() {
+        etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    searchPio();
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+    private void searchPio() {
+        String trim = etSearch.getText().toString().trim();
+        if (TextUtils.isEmpty(trim)){
+            ToastUtils.getInstance().makeToast(ToastUtils.ToastType.NORMAL).show("请输入搜索地址");
+        }else {
+            query = new PoiSearch.Query(trim, "", "");
+//keyWord表示搜索字符串，
+//第二个参数表示POI搜索类型，二者选填其一，选用POI搜索类型时建议填写类型代码，码表可以参考下方（而非文字）
+//cityCode表示POI搜索区域，可以是城市编码也可以是城市名称，也可以传空字符串，空字符串代表全国在全国范围内进行搜索
+            query.setPageSize(10);// 设置每页最多返回多少条poiitem
+            query.setPageNum(1);//设置查询页码
+            PoiSearch poiSearch = new PoiSearch(getContext(), query);
+            poiSearch.setOnPoiSearchListener(this);
+            poiSearch.searchPOIAsyn();
         }
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
 
-
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) ;//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE);//连续定位、且将视角移动到地图中心点，地图依照设备方向旋转，定位点会跟随设备移动。（1秒1次定位）
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
-//以下三种模式从5.1.0版本开始提供
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
-//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，地图依照设备方向旋转，并且蓝点会跟随设备移动。
-
-
-
-
-        myLocationStyle .showMyLocation(true);//设置是否显示定位小蓝点，用于满足只想使用定位，不想使用定位小蓝点的场景，设置false以后图面上不再有定位蓝点的概念，但是会持续回调位置信息。
-        aMap.setMyLocationEnabled(true);
-        aMap.setOnMyLocationChangeListener(this);
     }
 
-
-    public void myLocationIcon(BitmapDescriptor myLocationIcon) {
-        myLocationStyle. myLocationIcon (myLocationIcon);
-        ;//设置定位蓝点的icon图标方法，需要用到BitmapDescriptor类对象作为参数。
-    }
 
     @Override
     public void onMyLocationChange(Location location) {
         XLog.i(location.toString());
+
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult poiResult, int i) {
+        JLog.i(poiResult.toString());
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
 
     }
 }
