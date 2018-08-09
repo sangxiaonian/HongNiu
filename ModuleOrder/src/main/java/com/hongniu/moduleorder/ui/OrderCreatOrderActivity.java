940c914acfba61091c86eb89ef0b7c7a1cdc87a8
pingyapeng <pingyapeng@ehousechina.com>
2018-08-09 22:29:26 +0800

    细节调整

--- ModuleOrder/src/main/java/com/hongniu/moduleorder/ui/OrderCreatOrderActivity.java
+++ ModuleOrder/src/main/java/com/hongniu/moduleorder/ui/OrderCreatOrderActivity.java
@@ -7,6 +7,7 @@ import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.bigkoo.pickerview.TimePickerView;
@@ -21,6 +22,7 @@ import com.hongniu.moduleorder.control.OrderEvent;
 import com.hongniu.moduleorder.widget.CarNumPop;
 import com.sang.common.event.BusFactory;
 import com.sang.common.utils.JLog;
+import com.sang.common.utils.ToastUtils;
 import com.sang.common.widget.ItemView;
 import com.sang.common.widget.dialog.BottomAlertDialog;
 import com.sang.common.widget.dialog.builder.BottomAlertBuilder;
@@ -73,6 +75,8 @@ public class OrderCreatOrderActivity extends BaseActivity implements View.OnClic
     private ItemView itemDriverPhone;         //司机手机
     private TimePickerView timePickerView;
 
+    private Button btSave;
+
     private CarNumPop pop;
 
     @Override
@@ -99,6 +103,7 @@ public class OrderCreatOrderActivity extends BaseActivity implements View.OnClic
         itemCarPhone = findViewById(R.id.item_car_phone);
         itemCarName = findViewById(R.id.item_car_name);
         itemDriverName = findViewById(R.id.item_driver_name);
+        btSave = findViewById(R.id.bt_entry);
         timePickerView = PickerDialogUtils.initTimePicker(mContext, this, new boolean[]{true, true, true, false, false, false});
         pop = new CarNumPop(mContext);
 
@@ -110,7 +115,7 @@ public class OrderCreatOrderActivity extends BaseActivity implements View.OnClic
         itemStartTime.setOnClickListener(this);
         itemStartLocation.setOnClickListener(this);
         itemEndLocation.setOnClickListener(this);
-
+        btSave.setOnClickListener(this);
         itemCarNum.getEtCenter().addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -144,6 +149,9 @@ public class OrderCreatOrderActivity extends BaseActivity implements View.OnClic
         } else if (id == R.id.item_end_loaction) {
             ArouterUtils.getInstance().builder(ArouterParamOrder.activity_map_loaction).withBoolean(Param.TRAN, false).navigation(mContext);
 
+        }else if (id==R.id.bt_entry){
+            ToastUtils.getInstance().makeToast(ToastUtils.ToastType.SUCCESS).show();
+            finish();
         }
     }
 
