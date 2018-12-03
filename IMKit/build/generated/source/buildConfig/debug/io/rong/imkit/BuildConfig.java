064caa7bce01d849478b89cee06ab557f87e427c
pingyapeng <pingyapeng@ehousechina.com>
2018-12-03 10:36:34 +0800

    重新下载融云Module，修复融云官方引起的bug

--- IMKit/build/generated/source/buildConfig/debug/io/rong/imkit/BuildConfig.java
+++ IMKit/build/generated/source/buildConfig/debug/io/rong/imkit/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "io.rong.imkit";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2018112221;
+  public static final int VERSION_CODE = 2018113014;
   public static final String VERSION_NAME = "2.9.8 Dev";
 }
--- RCSticker/build/generated/not_namespaced_r_class_sources/debug/generateDebugRFile/out/android/support/mediacompat/R.java
+++ RCSticker/build/generated/not_namespaced_r_class_sources/debug/generateDebugRFile/out/android/support/mediacompat/R.java
@@ -141,7 +141,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static int status_bar_notification_info_overflow = 0x7f150225;
+        public static int status_bar_notification_info_overflow = 0x7f150222;
     }
     public static final class style {
         private style() {}
