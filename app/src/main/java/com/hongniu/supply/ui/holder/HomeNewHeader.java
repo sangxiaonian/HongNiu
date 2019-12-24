648240407d2c389a78c2fef38bc3d619089de11f
平亚鹏 <pingyapengwork@163.com>
2019-12-24 09:23:48 +0800

    2.6.1版本上线

--- app/src/main/java/com/hongniu/supply/ui/holder/HomeNewHeader.java
+++ app/src/main/java/com/hongniu/supply/ui/holder/HomeNewHeader.java
@@ -1,6 +1,9 @@
 package com.hongniu.supply.ui.holder;
 
 import android.content.Context;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.AbsoluteSizeSpan;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -26,7 +29,7 @@ public class HomeNewHeader extends PeakHolder implements View.OnClickListener {
     ViewGroup card_insurance;
     ViewGroup card_invite;
     private View.OnClickListener onClickListener;
-
+    private TextView tv_insurance;
 
 
     public HomeNewHeader(View itemView) {
@@ -56,6 +59,19 @@ public class HomeNewHeader extends PeakHolder implements View.OnClickListener {
         card_goods_match=itemView.findViewById(R.id.card_goods_match);
         card_insurance=itemView.findViewById(R.id.card_insurance);
         card_invite=itemView.findViewById(R.id.card_invite);
+        tv_insurance=itemView.findViewById(R.id.tv_insurance);
+
+        SpannableStringBuilder builder=new SpannableStringBuilder("货运险牛人保");
+        AbsoluteSizeSpan ab=new AbsoluteSizeSpan(11,true);
+        //文本字体绝对的大小
+        builder.setSpan(ab,3,6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv_insurance.setText(builder);
+
+
+
+        //进行隐藏
+        card_insurance.setVisibility(Utils.showInscance()?View.VISIBLE:View.GONE);
+
 
         ll_cargo.setOnClickListener(this);
         ll_car.setOnClickListener(this);
